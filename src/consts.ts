fb8ad79a7911a74cfaad74857bc4e808d9460daa
andmohiko <andmohiko@gmail.com>
2023-10-13 15:48:56 +0900

    del: src/content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'andmohiko.dev';
-export const SITE_DESCRIPTION = 'Portfolio | andmohiko';
+export const SITE_TITLE = 'andmohiko.dev'
+export const SITE_DESCRIPTION = 'Portfolio | andmohiko'
