c6e7c210170a2e2f45fdee9925afb2affd3c5c77
andmohiko <andmohiko@gmail.com>
2023-11-14 17:28:34 +0900

    feat: workcard design

--- src/types/work.ts
+++ src/types/work.ts
@@ -1,13 +1,14 @@
 export type Work = {
   id: string
   body: string
+  description: string
   link: string
   publishAt: string
   tags: string[]
   thumbnail: {
-    height: number;
-    url: string;
-    width: number;
+    height: number
+    url: string
+    width: number
   }
   title: string
 }
