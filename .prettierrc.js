41f6422e885e13583380d4779d2f003b8e2919b5
andmohiko <andmohiko@gmail.com>
2023-11-23 17:44:10 +0900

    feat: set astro eslint and prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,15 @@ module.exports = {
   semi: false,
   singleQuote: true,
   trailingComma: 'all',
+  plugins: [require.resolve('prettier-plugin-astro')],
+  tabWidth: 2,
+  useTabs: true,
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
 }
